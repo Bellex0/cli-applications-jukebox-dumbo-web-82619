5fa1b364e212bd99c383ff12709b3ea3f81a31c1
Belle P <bellepx3@gmail.com>
2019-07-14 05:52:27 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -20,7 +20,5 @@ puts  "- exit : exits this program"
 end
 
 def list (songs)
-songs.each_with_index {|song, index|
-  puts "#{index+1}. #{song}"
-}
+songs.each_with_index {|song, index| puts "#{index+1}. #{song}"}
 end
